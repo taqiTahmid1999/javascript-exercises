df20389192f0e620db3d7fe6ea3ee027966e9a75
taqiTahmid1999 <tahmidtaqi1999@gmail.com>
2024-04-09 13:03:48 +0600

    7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  /*
+  Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+   Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9.
+   */
+  return (Fahrenheit-32)*(5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  return (Celsius*(9/5))+32;
 };
 
 // Do not edit below this line
